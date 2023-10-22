c27a2d7e7cc6e531e46a9b61156dfcf95fe53cf1
Muhammad Angga Kusuma <kusumaangga719@gmail.com.com>
2023-10-22 23:26:47 +0700

    Vidio 5

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://prakweb2023_b_213040074_mvc.test/phpmvc/public');
--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container-fluid">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/Me.jpg" alt="Muhammad Angga Kusuma" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
